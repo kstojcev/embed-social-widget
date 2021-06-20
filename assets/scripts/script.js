69ae70c0578e54433fb4ffcd0faf98f63b18f1eb
Kristijan Stojcev <stojcev_kristijan@hotmail.com>
2021-06-20 17:42:02 +0200

    Made logic for source checking and starting logic for date

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,21 @@ fetch("assets/data.json")
     let cardsContainer = document.getElementById("cardsContainer");
 
     for (let i = 0; i < cardInfo.length; i++) {
-      console.log(cardInfo[i].caption);
+      str = `${cardInfo[i].date}`;
+      strSplit = str.split(" ");
+      strSlice = strSplit.slice();
+      //   console.log(strSlice[0]);
+      neededString = strSlice[0];
+
+      function sourceCheck() {
+        if (cardInfo[i].source_type == "facebook") {
+          return "./assets/icons/icons/facebook.svg";
+        } else {
+          return "./assets/icons/icons/instagram-logo.svg";
+        }
+      }
+
+      //   console.log(cardInfo[i].caption);
       let html = `<div class="card" id="modalBtn">
       <div class="cardHeader">
         <div class="headerTitle">
@@ -18,7 +32,9 @@ fetch("assets/data.json")
             <p>${cardInfo[i].date}</p>
           </div>
         </div>
-        <img src="./assets/icons/icons/instagram-logo.svg" alt="" />
+        <a href='${cardInfo[i].source_link}'>
+        <img src="${sourceCheck()}" alt="" />
+        </a>
       </div>
 
       <div class="cardContent">
